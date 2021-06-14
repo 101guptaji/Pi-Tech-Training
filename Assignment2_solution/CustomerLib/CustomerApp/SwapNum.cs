4178612629629c634780f971a2f6324eb1864c9f
Himanshu Gupta <101guptaji@gmail.com>
2021-06-14 10:43:17 +0530

    Swap using generic class

---
+++ Assignment2_solution/CustomerLib/CustomerApp/SwapNum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp
+{
+    class SwapNum
+    {
+        static void Main(string[] args)
+        {
+            SwapNumGeneric<int> num1 = new SwapNumGeneric<int>(12, 55);
+            Console.WriteLine("Before Swap: "+num1.toString());
+            num1.swap();
+
+            Console.WriteLine("After Swap: " + num1.toString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment2_solution/CustomerLib/CustomerApp/SwapNumGeneric.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CustomerApp
+{
+    
+    public class SwapNumGeneric<@int>
+    {
+         
+
+        private @int first;
+
+        public @int First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+
+        private @int second;
+
+        public @int Second
+        {
+            get { return second; }
+            set { second = value; }
+        }
+
+
+        public SwapNumGeneric(@int firstElement, @int secondElement)
+            {
+                First = firstElement;
+                Second = secondElement;
+            }
+
+
+ 
+            public void swap()
+            {
+
+                @int temp = First;
+                First = Second;
+                Second = temp;
+            }
+        //public SwapNumGeneric<T, T> swap()
+        //    {
+        //        return new SwapNumGeneric(second, first);
+        //    }
+
+            public string toString() { return string.Format($"{first}, {second}"); }
+        }
+}
+
